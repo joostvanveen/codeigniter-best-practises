c95370a7552ec9d13c40508994c84e26045942ed
Joost van Veen <joost@accentinteractive.nl>
2013-02-27 13:59:44 +0100

    Initial commit. CI downloaded

---
+++ application/logs/log-2013-02-27.php
@@ -0,0 +1,36 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+DEBUG - 2013-02-27 12:03:34 --> Config Class Initialized
+DEBUG - 2013-02-27 12:03:34 --> Hooks Class Initialized
+DEBUG - 2013-02-27 12:03:34 --> Utf8 Class Initialized
+DEBUG - 2013-02-27 12:03:34 --> UTF-8 Support Enabled
+DEBUG - 2013-02-27 12:03:34 --> URI Class Initialized
+DEBUG - 2013-02-27 12:03:34 --> Router Class Initialized
+DEBUG - 2013-02-27 12:03:34 --> No URI present. Default controller set.
+DEBUG - 2013-02-27 12:03:34 --> Output Class Initialized
+DEBUG - 2013-02-27 12:03:34 --> Security Class Initialized
+DEBUG - 2013-02-27 12:03:34 --> Input Class Initialized
+DEBUG - 2013-02-27 12:03:34 --> Global POST and COOKIE data sanitized
+DEBUG - 2013-02-27 12:03:34 --> Language Class Initialized
+DEBUG - 2013-02-27 12:03:34 --> Loader Class Initialized
+DEBUG - 2013-02-27 12:03:34 --> Controller Class Initialized
+DEBUG - 2013-02-27 12:03:34 --> File loaded: ../application/views/welcome_message.php
+DEBUG - 2013-02-27 12:03:34 --> Final output sent to browser
+DEBUG - 2013-02-27 12:03:34 --> Total execution time: 0.0145
+DEBUG - 2013-02-27 13:47:52 --> Config Class Initialized
+DEBUG - 2013-02-27 13:47:52 --> Hooks Class Initialized
+DEBUG - 2013-02-27 13:47:52 --> Utf8 Class Initialized
+DEBUG - 2013-02-27 13:47:52 --> UTF-8 Support Enabled
+DEBUG - 2013-02-27 13:47:52 --> URI Class Initialized
+DEBUG - 2013-02-27 13:47:52 --> Router Class Initialized
+DEBUG - 2013-02-27 13:47:52 --> No URI present. Default controller set.
+DEBUG - 2013-02-27 13:47:52 --> Output Class Initialized
+DEBUG - 2013-02-27 13:47:52 --> Security Class Initialized
+DEBUG - 2013-02-27 13:47:52 --> Input Class Initialized
+DEBUG - 2013-02-27 13:47:52 --> Global POST and COOKIE data sanitized
+DEBUG - 2013-02-27 13:47:52 --> Language Class Initialized
+DEBUG - 2013-02-27 13:47:52 --> Loader Class Initialized
+DEBUG - 2013-02-27 13:47:52 --> Controller Class Initialized
+DEBUG - 2013-02-27 13:47:52 --> File loaded: ../application/views/welcome_message.php
+DEBUG - 2013-02-27 13:47:52 --> Final output sent to browser
+DEBUG - 2013-02-27 13:47:52 --> Total execution time: 0.1138
